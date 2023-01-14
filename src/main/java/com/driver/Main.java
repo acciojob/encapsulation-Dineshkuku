974475bffa6ec49e4ae7e6349329d979207f9157
Dinesh Kukreja <dhirensharma540@gmail.com>
2023-01-14 11:33:24 +0530

    added source code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        rw.setStr("Dinesh");
+        System.out.print(rw.getStr());
+        
+
+        // Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+        // The field RWOnly.str is not visible
+        // The field RWOnly.str is not visible
+
+    }
 }
