267ac3c6933f7a2a1214ea36e20db40483c01bbb
Dinesh Kukreja <dhirensharma540@gmail.com>
2023-01-14 11:48:11 +0530

    updated src

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly rw = new RWOnly();
-        rw.setStr("Dinesh");
-        System.out.print(rw.getStr());
-        
+        rw.setName("Dinesh");
+        System.out.println(rw.getName());
+
 
         // Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
         // The field RWOnly.str is not visible
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,14 +1,29 @@
 package com.driver;
 
-public class RWOnly {
+public class RWOnly {   
     private String str;
 
-    public String getStr() {
+    public String getName() {
         return str;
     }
-
-    public void setStr(String str) {
+    public void setName(String str) {
         this.str = str;
     }
-    
+
 }
+
+
+// package com.driver;
+
+// public class RWOnly {
+//     private String ma;
+//     public String getName()
+//     {
+//         return ma;
+//     }
+//     public void setName(String ma)
+//     {
+//         this.ma = ma;
+//     }
+
+// }
